518479eb197958f85fdfa8bf5a889883c23572b1
Simeon Lee <ipzs3mini@gmail.com>
2016-11-07 02:13:25 +0000

    Adding Project Files
    Adding Project Files from a previously completed project.

---
+++ SystemInfo - Final/SystemInfo/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SystemInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelIP = new System.Windows.Forms.Label();
+            this.labelOS = new System.Windows.Forms.Label();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPC = new System.Windows.Forms.TextBox();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxOS = new System.Windows.Forms.TextBox();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(105, 203);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 0;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(17, 64);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(67, 17);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "PC Name";
+            // 
+            // labelIP
+            // 
+            this.labelIP.AutoSize = true;
+            this.labelIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIP.Location = new System.Drawing.Point(17, 97);
+            this.labelIP.Name = "labelIP";
+            this.labelIP.Size = new System.Drawing.Size(76, 17);
+            this.labelIP.TabIndex = 2;
+            this.labelIP.Text = "IP Address";
+            // 
+            // labelOS
+            // 
+            this.labelOS.AutoSize = true;
+            this.labelOS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOS.Location = new System.Drawing.Point(17, 130);
+            this.labelOS.Name = "labelOS";
+            this.labelOS.Size = new System.Drawing.Size(69, 17);
+            this.labelOS.TabIndex = 3;
+            this.labelOS.Text = "OS Name";
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUser.Location = new System.Drawing.Point(17, 163);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(79, 17);
+            this.labelUser.TabIndex = 4;
+            this.labelUser.Text = "User Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "System Info";
+            // 
+            // textBoxPC
+            // 
+            this.textBoxPC.Location = new System.Drawing.Point(105, 63);
+            this.textBoxPC.Name = "textBoxPC";
+            this.textBoxPC.Size = new System.Drawing.Size(153, 20);
+            this.textBoxPC.TabIndex = 10;
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(105, 96);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(153, 20);
+            this.textBoxIP.TabIndex = 11;
+            // 
+            // textBoxOS
+            // 
+            this.textBoxOS.Location = new System.Drawing.Point(105, 129);
+            this.textBoxOS.Name = "textBoxOS";
+            this.textBoxOS.Size = new System.Drawing.Size(153, 20);
+            this.textBoxOS.TabIndex = 12;
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(105, 162);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(153, 20);
+            this.textBoxUser.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.textBoxOS);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.textBoxPC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelUser);
+            this.Controls.Add(this.labelOS);
+            this.Controls.Add(this.labelIP);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonOk);
+            this.Name = "Form1";
+            this.Text = "System Info";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelIP;
+        private System.Windows.Forms.Label labelOS;
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPC;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxOS;
+        private System.Windows.Forms.TextBox textBoxUser;
+    }
+}
+
---
+++ SystemInfo - Final/SystemInfo/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace SystemInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // PC Name
+            string pc = Environment.MachineName;
+            textBoxPC.Text = "pc";
+                        
+                                  
+            // User Name              
+            string user = Environment.UserName;
+            textBoxUser.Text = user;
+
+
+            // IP Address
+            string hostName = Dns.GetHostName(); 
+            string ip = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            textBoxIP.Text = ip;
+
+
+            // OS
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows NT\\CurrentVersion");
+            string pathName = (string)registryKey.GetValue("productName");
+            textBoxOS.Text = pathName;
+                       
+        }
+
+        //        
+        //Button "OK". Closes application.
+        //
+        private void buttonOk_Click(object sender, EventArgs e)
+            {
+                this.Close();
+            }
+                   
+                
+     }
+}
